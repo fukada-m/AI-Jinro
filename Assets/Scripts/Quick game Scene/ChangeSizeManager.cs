d043eec1f2cc02ed5348c219965b89fadb9c74aa
fukada <fukada.m888@gmail.com>
2025-08-30 22:54:20 +0900

    初期表示する丸は大きくする

--- Assets/Scripts/Quick game Scene/ChangeSizeManager.cs
+++ Assets/Scripts/Quick game Scene/ChangeSizeManager.cs
@@ -10,6 +10,12 @@ public class ChangeSizeManager : MonoBehaviour
     [SerializeField]
     ChangeSize[] _changeSizes = new ChangeSize[7];
 
+    void Start()
+    {
+        // 初期表示はお題
+        _changeSizes[0].SizeUp(_bigSize);
+    }
+
     // クリックを検知したら全ての丸を一度標準サイズにしてからクリックされた対象だけを拡大する
     public void DetctionClick(ChangeSize cz)
     {
