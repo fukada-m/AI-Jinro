cefa5907ea86e033f092b84a0b03435684236a98
fukada <fukada.m888@gmail.com>
2025-08-30 22:53:58 +0900

    掲示板の管理はboardManagerに任せる

--- Assets/Scripts/Quick game Scene/GameManager.cs
+++ Assets/Scripts/Quick game Scene/GameManager.cs
@@ -3,23 +3,23 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Text _themeText; // お題を表示する
+    // [SerializeField] Text _themeText; // お題を表示する
+    [SerializeField] BoadManager _boardManager; // 掲示板を管理する
     [SerializeField] CountdownTimer _countdownTimer;  // 残り秒数を表示する
     [SerializeField] MessageContext _messageContext;  // メッセージのステートパターン
     [SerializeField] ChatState _chatState; // チャットステート
     [SerializeField] SubjectState _subjectState; // お題ステート
 
     // お題をセットする
-    public void SetTheme(string theme)
-    {
-        _themeText.text = theme;
-    }
+    // public void SetTheme(string theme)
+    // {
+    //     _themeText.text = theme;
+    // }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // お題を仮表示。本来はAIにその都度考えてもらう
-        SetTheme("パチパチはじけるもの");
+        _boardManager.Initialize();
         _countdownTimer.StartCountdown(); // カウントダウンスタート
         _messageContext.SetState(_subjectState); // 最初はお題ステート
     }
