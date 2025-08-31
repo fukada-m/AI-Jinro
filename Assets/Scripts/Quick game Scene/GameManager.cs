413bc05f3aaf947d69cbe1269a868e098903aa94
fukada <fukada.m888@gmail.com>
2025-08-31 15:18:57 +0900

    コメント編集

--- Assets/Scripts/Quick game Scene/GameManager.cs
+++ Assets/Scripts/Quick game Scene/GameManager.cs
@@ -3,23 +3,16 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // [SerializeField] Text _themeText; // お題を表示する
     [SerializeField] BoadManager _boardManager; // 掲示板を管理する
     [SerializeField] CountdownTimer _countdownTimer;  // 残り秒数を表示する
     [SerializeField] MessageContext _messageContext;  // メッセージのステートパターン
     [SerializeField] ChatState _chatState; // チャットステート
     [SerializeField] SubjectState _subjectState; // お題ステート
 
-    // お題をセットする
-    // public void SetTheme(string theme)
-    // {
-    //     _themeText.text = theme;
-    // }
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _boardManager.Initialize();
+        _boardManager.Initialize();  // 掲示板を初期化
         _countdownTimer.StartCountdown(); // カウントダウンスタート
         _messageContext.SetState(_subjectState); // 最初はお題ステート
     }
