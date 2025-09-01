1a2f7db5e2893f277005d1fc3b19012cce6b2b14
fukada <fukada.m888@gmail.com>
2025-09-01 21:56:16 +0900

    お題を投稿する機能

--- Assets/Scripts/Quick game Scene/Boad.cs
+++ Assets/Scripts/Quick game Scene/Boad.cs
@@ -2,25 +2,26 @@ using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
+// 掲示板を管理するクラス
 public class Boad : MonoBehaviour
 {
-    public int CurrentIndex;
-    [SerializeField] Circle[] _circles = new Circle[7];
-    string[] _textArr = new string[7]; // ボードテキストの配列7個ある
+    public int CurrentIndex; // 今どのボードを表示しているか管理する
+    [SerializeField] Text _board; //ボードに表示する文字列
+    [SerializeField] Circle[] _circles = new Circle[7]; // 丸の配列 7 個ある
+    string[] _textArr = new string[7]; // ボードに表示するテキストの配列 7 個ある
 
-
-    [SerializeField]
-    // 現在のボード
-    Text _board;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         CurrentIndex = 0; //初期インデックスは 0
+        // イベントを購読。丸がクリックされたら表示される内容を丸に対応するものに変更
         foreach (var circle in _circles)
         {
             circle.OnClicked
-                .Subscribe(circle => CurrentIndex = circle.Index)
+                .Subscribe(circle =>
+                {
+                    CurrentIndex = circle.Index;
+                    Display(CurrentIndex);
+                })
                 .AddTo(this);
         }
     }
@@ -33,11 +34,16 @@ public class Boad : MonoBehaviour
         _board.text = _textArr[CurrentIndex]; // 最初は[0]を表示 
     }
 
-    // Update is called once per frame
-    void Update()
+    // ボードに文字列をセットし表示するメソッド
+    public void SetText(int i, string s)
     {
-
+        _textArr[i] = s;
+        Display(i);
     }
-    
-    // ボードを表示するメソッド
+
+    // ボードに表示するメソッド
+    public void Display(int i)
+    {
+        _board.text = _textArr[i];
+    }
 }
--- Assets/Scripts/Quick game Scene/Message/SubjectState.cs
+++ Assets/Scripts/Quick game Scene/Message/SubjectState.cs
@@ -1,11 +1,20 @@
-using System;
 using UnityEngine;
+using UnityEngine.UI;
 
+// お題への回答を投稿するステート
 public class SubjectState : MonoBehaviour, IMessageState
 {
+    [SerializeField] Boad _board;  // ボード
+    [SerializeField] InputField inputField;  // 入力欄
+
+    // メッセージを掲示板に送って入力欄をクリア
     public void SendMessage()
     {
-        // TODO 処理を実装する
-        Debug.Log("お題を回答");
+        string text = inputField.text;
+        if (string.IsNullOrWhiteSpace(text)) return;
+
+        _board.SetText(1, text);
+
+        inputField.text = ""; // 入力欄をクリア
     }
 }
