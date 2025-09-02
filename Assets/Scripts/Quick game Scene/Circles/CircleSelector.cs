80cae3f0b731b07d1bf9f4e924ec0ae495aedaaa
fukada <fukada.m888@gmail.com>
2025-09-03 08:22:57 +0900

    次へ、前へボタンと選択している丸を連動

--- Assets/Scripts/Quick game Scene/Circles/CircleSelector.cs
+++ Assets/Scripts/Quick game Scene/Circles/CircleSelector.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class CircleSelector : MonoBehaviour
 {
+    public int CurrentCircle;
     [SerializeField] Vector2 _bigSize; // 拡大したときのサイズ
     [SerializeField] Circle[] _circles = new Circle[7]; // 拡大縮小を管理する丸が入った配列
 
     void Start()
     {
+        CurrentCircle = 0;
         _circles[0].ChangeSize(_bigSize); // 初期表示時はお題の丸を拡大
 
         // 7個のサークルのイベントを購読
@@ -19,9 +21,30 @@ public class CircleSelector : MonoBehaviour
                 {
                     foreach (var c in _circles) c.SizeInitialize();
                     circle.ChangeSize(_bigSize);
+                    CurrentCircle = circle.Index;
                 })
                 .AddTo(this);
         }
+    }
+
+    public void Next()
+    {
+        if (CurrentCircle < 6) CurrentCircle++;
+        foreach (var c in _circles)
+        {
+            c.SizeInitialize();
+        }
+        _circles[CurrentCircle].ChangeSize(_bigSize);
+    }
+
+    public void Back()
+    {
+        if (CurrentCircle > 0) CurrentCircle--;
+        foreach (var c in _circles)
+        {
+            c.SizeInitialize();
+        }
+        _circles[CurrentCircle].ChangeSize(_bigSize);
     }
 
 }
