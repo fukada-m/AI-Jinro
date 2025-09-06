133f19982c17a6ce66d78938acafbfbd122c0ac4
fukada <fukada.m888@gmail.com>
2025-09-06 23:28:43 +0900

    お題に回答できるのは一度だけ

--- Assets/Scripts/Quick game Scene/Message/SubjectState.cs
+++ Assets/Scripts/Quick game Scene/Message/SubjectState.cs
@@ -6,15 +6,27 @@ public class SubjectState : MonoBehaviour, IMessageState
 {
     [SerializeField] Board _board;  // ボード
     [SerializeField] InputField inputField;  // 入力欄
+    [SerializeField] FlashMessage _flashMessage; // フラッシュメッセージ
+    bool _isLocked = false; // お題に回答できるのは1度だけ
 
     // メッセージを掲示板に送って入力欄をクリア
     public void SendMessage()
     {
-        string text = inputField.text;
-        if (string.IsNullOrWhiteSpace(text)) return;
+        if (!_isLocked)
+        {
+            string text = inputField.text;
+            if (string.IsNullOrWhiteSpace(text)) return;
 
-        _board.SetText(1, text);
+            _board.SetText(1, text);
 
-        inputField.text = ""; // 入力欄をクリア
+            inputField.text = ""; // 入力欄をクリア
+            _flashMessage.ShowMessage("お題に回答しました");
+            _isLocked = true;
+        }
+        else
+        {
+            inputField.text = ""; // 入力欄をクリア
+            _flashMessage.ShowMessage("既に回答済みです");
+        }
     }
 }
