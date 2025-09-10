a1fbd6395398cd709ae71c24e7b254cbe74fc44d
fukada <fukada.m888@gmail.com>
2025-09-10 09:33:13 +0900

    名称変更とフェーズの管理に特化

--- Assets/Scripts/Quick game Scene/PhaseController.cs
+++ Assets/Scripts/Quick game Scene/PhaseController.cs
@@ -1,50 +1,53 @@
 using UniRx;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class PhaseController : MonoBehaviour
 {
-    [SerializeField] Board _board; // 掲示板
     [SerializeField] FlashMessage _flashMessage; // フラッシュメージ
     [SerializeField] CountdownTimer _countdownTimer;  // 残り秒数を表示する
     [SerializeField] MessageContext _messageContext;  // メッセージのステートパターン
     [SerializeField] ChatState _chatState; // チャットステート
     [SerializeField] SubjectState _subjectState; // お題ステート
-    [SerializeField] int _subjectTime;
-    [SerializeField] int _chatTime;
-    string _currentPhase;
+    [SerializeField] int _subjectTime; // お題フェーズの時間
+    [SerializeField] int _chatTime; // チャットフェーズの時間
+    string _currentPhase; 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _currentPhase = "お題";
-        _board.Initialize();  // 掲示板を初期化
         _countdownTimer.EndCount.Subscribe(isCounting => ChangePhase()).AddTo(this); // カウントダウンが終わると通知されるイベントを購読
         _countdownTimer.StartCountdown(_subjectTime); // カウントダウンスタート
         _messageContext.SetState(_subjectState); // 最初はお題ステート
         _flashMessage.ShowMessage("お題に答えよう");
     }
 
-    // フェーズは お題 ⇒ チャット⇒ 投票
+    // フェーズは お題 ⇒ チャット⇒ 投票 ⇒ 結果発表
     void ChangePhase()
     {
+        // お題 ⇒ チャット
         if (_currentPhase == "お題")
         {
             ChangeChatState();
             _currentPhase = "チャット";
-        } else if (_currentPhase == "チャット")
+        }
+        // チャット ⇒ 投票
+        else if (_currentPhase == "チャット")
+        {
+            _currentPhase = "投票";
+
+        }
+        // 投票 ⇒ 結果発表
+        else if (_currentPhase == "投票")
         {
 
         }
-
     }
-    // お題フェーズのカウントダウンが終了したらチャットステートに変える
+
+    // チャットステートに遷移
     void ChangeChatState()
     {
         _messageContext.SetState(_chatState);
         _flashMessage.ShowMessage("誰がAIなのかチャットで話し合おう");
         _countdownTimer.StartCountdown(_chatTime);
     }
-    
-
 }
