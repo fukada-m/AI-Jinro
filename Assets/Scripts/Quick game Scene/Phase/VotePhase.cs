3f63987a7f5f05a66455666007e905235994e232
fukada <fukada.m888@gmail.com>
2025-09-12 20:07:17 +0900

    一応投票完了処理を追加

--- Assets/Scripts/Quick game Scene/Phase/VotePhase.cs
+++ Assets/Scripts/Quick game Scene/Phase/VotePhase.cs
@@ -5,21 +5,34 @@ public class VotePhase : PhaseBase
 {
     IMessageState _voteState;
     Vote _vote;
-    [SerializeField] AiManager _aiManager;
+    AiManager _aiManager;
 
     protected override void Awake()
     {
         base.Awake();
         _voteState = GetComponent<SubjectState>();
-        if (_voteState == null) Debug.LogError("SubjectStateクラスがGetComponentできなかった");
+        if (_voteState == null) Debug.LogError("SubjectStateクラスがGetComponentできませんでした");
         _vote = GetComponent<Vote>();
-        if (_vote == null) Debug.LogError("VoteクラスがGetComponentできなかった");
+        if (_vote == null) Debug.LogError("VoteクラスがGetComponentできませんでした");
+        _aiManager = GetComponent<AiManager>();
+        if (_aiManager == null) Debug.LogError("AiManagerクラスがGetComponentできませんでした");
+
     }
 
-    // AIに投票させる
+    // AIに投票させる 
+    // TODO 非同期メソッドにして終わったら投票完了にする。今はUpdateで監視してる
     protected override void AiAction()
     {
         _aiManager.Vote();
+    }
+
+    void Update()
+    {
+        if (_aiManager.VoteDone)
+        {
+            Debug.Log("投票完了");
+            _aiManager.VoteDone = false;
+        }
     }
 
     public override void ChangePhase()
