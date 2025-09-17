375086181713fe14e77eb9f54e521dd8f1db4d24
fukada <fukada.m888@gmail.com>
2025-09-17 22:37:49 +0900

    勝敗判定処理を追加

--- Assets/Scripts/Quick game Scene/Phase/JudgePhase.cs
+++ Assets/Scripts/Quick game Scene/Phase/JudgePhase.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 // 勝敗判定フェーズ
@@ -20,7 +21,7 @@ public class JudgePhase : PhaseBase
     {
         base.ChangePhase();
         SetMessageState();
-        Judge();
+        StartCoroutine(Judge());
     }
 
     protected override void SetMessageState()
@@ -28,16 +29,32 @@ public class JudgePhase : PhaseBase
         _messageContext.SetState(_judgeState);
     }
 
-    void Judge()
+    // 勝敗判定処理
+    IEnumerator Judge()
     {
-        // プレイヤー1がなければ人の負け
-        string circle1Name = _board.Circles[1].gameObject.name;
-        if (circle1Name != "Circle (1)") _flashMessage.ShowMessage("あなたの負けです");
+        yield return new WaitForSeconds(1f);
 
-        // Circleが3つなら人の勝ち
-        if (_board.Circles.Count == 3) _flashMessage.ShowMessage("あなたの勝ちです");
+        // フラッシュメッセージを表示するための準備
+        GameObject flashMessageOBJ = Instantiate(_flashMessagePrefab, _canvasTransform);
+        FlashMessage flashMessage = flashMessageOBJ.GetComponent<FlashMessage>();
 
-        // それ以外は続行
-        _flashMessage.ShowMessage("続行します");
+        string circle1Name = _board.Circles[1].gameObject.name; // 2個目の丸の名前を取得
+
+        // プレイヤー1の丸がなければ人の負け
+        if (circle1Name != "Circle (1)")
+        {
+            flashMessage.ShowMessage("あなたの負けです");
+        }
+        // 残ってるCircleが3つなら人の勝ち
+        else if (_board.Circles.Count == 3)
+        {
+            flashMessage.ShowMessage("あなたの勝ちです");
+        }
+        // それ以外はゲーム続行
+        else
+        {
+            flashMessage.ShowMessage("AIが1人脱落しました。ゲームは続きます。");
+        }
+
     }
 }
